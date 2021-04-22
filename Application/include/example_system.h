f21e6557fb339b5ea8835908aad5f70b888c33c6
NVriezen <36536400+NVriezen@users.noreply.github.com>
2021-04-22 14:28:52 +0200

    Removed rendering & resources example code

--- Application/include/example_system.h
+++ Application/include/example_system.h
@@ -4,16 +4,11 @@
 
 const char* example_system_name = "example_system";
 
-float speed = 100;
-
 void example_system_update(zel_level_t* level, float delta_time)
 {
 	for (zel_entity_id entity : zel_entities_list<zel_transform_t>(level))
 	{
-		zel_transform_t* transform = zel_level_get_component<zel_transform_t>(level, entity);
-		
-		//transform->rotation.x += speed * delta_time;
-		transform->rotation.y += speed * delta_time;
-		//transform->rotation.z += speed * delta_time;
+		zel_level_get_component<zel_transform_t>(level, entity)->position.x += 1;
+		//zel_print("New transform X position | entity [%d]: %0.1f\n", entity, zel_level_get_component<zel_transform_t>(level, entity)->position.x);
 	}
 }
