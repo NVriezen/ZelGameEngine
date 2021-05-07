faf08284cb7fe66404c403643b3083b05ed154a2
NVriezen <36536400+NVriezen@users.noreply.github.com>
2021-05-07 14:40:10 +0200

    Example code for how the playtest can look at the end.

--- Application/include/example_system.h
+++ Application/include/example_system.h
@@ -4,11 +4,28 @@
 
 const char* example_system_name = "example_system";
 
+struct player_t {
+
+};
+
 void example_system_update(zel_level_t* level, float delta_time)
 {
-	for (zel_entity_id entity : zel_entities_list<zel_transform_t>(level))
+	for (zel_entity_id entity : zel_entities_list<player_t>(level))
 	{
-		zel_level_get_component<zel_transform_t>(level, entity)->position.x += 1;
-		//zel_print("New transform X position | entity [%d]: %0.1f\n", entity, zel_level_get_component<zel_transform_t>(level, entity)->position.x);
+		zel_transform_t* player_transform = zel_level_get_component<zel_transform_t>(level, entity);
+
+		int state = zel_input_get_key_hold(ZEL_KEY_A);
+		if (state)
+		{
+			player_transform->position.x -= delta_time;
+		}
+
+		state = zel_input_get_key_hold(ZEL_KEY_D);
+		if (state)
+		{
+			player_transform->position.x += delta_time;
+		}
+
+		zel_print("Player is position: { %f, %f, %f }\n", player_transform->position.x, player_transform->position.y, player_transform->position.z);
 	}
 }
--- Application/source/zel_entry.cpp
+++ Application/source/zel_entry.cpp
@@ -18,20 +18,34 @@ void zel_initialization()
 
 	// Level Initialization - TODO: put this somewhere else (Level manager?)
 	zel_print("Hello World\n");
-	zel_level_t* example_level = zel_level_create("Example Level");
+
+	zel_level_t* example_level = zel_level_create("Playtest Example Level");
 	active_level = example_level;
 
-	zel_entity_id entity = zel_level_create_entity(active_level);
+	zel_level_register_component<zel_transform_t>(example_level);
+	zel_level_register_component<player_t>(example_level);
 
-	// Registering Components
-	zel_level_register_component<zel_transform_t>(active_level);
+	zel_entity_id player_entity = zel_level_create_entity(example_level);
+	zel_entity_id enemy1_entity = zel_level_create_entity(example_level);
+	zel_entity_id enemy2_entity = zel_level_create_entity(example_level);
 
-	// Transform
-	zel_transform_t transform{ { 0.0f, 0.0f, 0.0f },{ 0.0f, 0.0f, 0.0f },{ 1.0f, 1.0f, 1.0f } };
-	zel_level_add_component(active_level, entity, transform);
+	zel_transform_t player_transform;
+	player_transform.position = {0, 128, 0};
+	zel_level_add_component<zel_transform_t>(example_level, player_entity, player_transform);
 
-	//Register System
-	zel_level_register_system(active_level, example_system_update, example_system_name);
+	player_t player_tag;
+	zel_level_add_component<player_t>(example_level, player_entity, player_tag);
+
+	zel_transform_t enemy1_transform;
+	enemy1_transform.position = { -64, 0, 0 };
+
+	zel_transform_t enemy2_transform;
+	enemy2_transform.position = { 64, 0, 0 };
+
+	zel_level_add_component<zel_transform_t>(example_level, enemy1_entity, enemy1_transform);
+	zel_level_add_component<zel_transform_t>(example_level, enemy2_entity, enemy2_transform);
+
+	zel_level_register_system(example_level, example_system_update, example_system_name);
 }
 
 // Called every frame. Put your logic here.
